4d8453328aacd5f0b7af3767605caef969fa85b8
azazel <azaz3l0z@protonmail.com>
2022-05-10 09:42:25 +0200

    First working ver full

---
+++ js/pythonScripts/setup.sh
@@ -0,0 +1,11 @@
+python3 -m venv ./venv
+. ./venv/bin/activate
+pip install pyinstaller
+pip install -r requirements.txt
+pyinstaller --onefile pyScraper.py
+yes | mv ./dist/* .
+yes | rm -r ./dist
+yes | rm -r ./build
+yes | rm pyScraper.spec
+deactivate
+yes | rm -r ./venv
---
+++ js/pythonScripts/test.py
@@ -0,0 +1,2 @@
+t = {1: 2}
+print(t.get(1).get(2).get(3))
